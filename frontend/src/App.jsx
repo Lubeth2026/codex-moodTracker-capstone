95c166fa891e782e3038978d42e2a758f1bdd724
Lubeth <lubethjimenez@yahoo.com>
2026-07-26 12:27:53 -0400

    Add npm install react-router-dom, wrapped BrowserRouter to project in main.jsx & started implementing a Welcome route as the Home page in React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 
 function App() {
@@ -7,8 +8,11 @@ function App() {
   return (
     <>
       <h1>Mood Tracker</h1>
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
