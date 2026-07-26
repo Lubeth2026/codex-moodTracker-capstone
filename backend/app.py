7620289f8ca91555b95109fe20fa916d8b78ffc6
Lubeth <lubethjimenez@yahoo.com>
2026-07-26 10:59:22 -0400

    Add POST create_mood route to CREATE new mood track w/ the UI entered & insert new mood in supabase table

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 
 import os 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 from supabase import create_client, Client
@@ -23,6 +23,17 @@ def get_moods():
 
     response = (supabase.table("user_moods").select("*").execute())
     return response.data
+
+@app.post("/api/moods")
+def create_mood():
+    data = request.get_json()
+
+    response = (supabase.table("user_moods").insert({
+            "mood": data["mood"],
+            "mood_scale": data["mood_scale"],
+            "notes": data["notes"]
+        }).execute())
+    return response.data, 201
 
 if __name__ == '__main__':
     app.run(debug=True)
