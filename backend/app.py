218e5869f4205de0b1a84659e73d2f09a6eaeeb1
Lubeth <lubethjimenez@yahoo.com>
2026-07-26 10:42:06 -0400

    Add GET get_moods route to READ all the moods data in supabase & retrieve to display in app

--- backend/app.py
+++ backend/app.py
@@ -15,8 +15,14 @@ supabase: Client = create_client(
 )
 
 @app.get("/")
-def getStatus():
+def get_Status():
     return {"status": "Good/Working"}
+
+@app.get("/api/moods")
+def get_moods():
+
+    response = (supabase.table("user_moods").select("*").execute())
+    return response.data
 
 if __name__ == '__main__':
     app.run(debug=True)
